74fb4ede5478fb89a93dc369a1f03de6c6310c99
Technados <117394920+Technados@users.noreply.github.com>
2025-03-05 10:23:01 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "XgHUbk2GMvgzt8OBoh4OGKLF0mHC80itQiFjpeJnkcA4P7uO3RuC0SlbTpMFTreF";
 /**
  * Get list of teams in event
  *
